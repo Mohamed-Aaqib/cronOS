eaba763f2e0348da681eff65a3c337f35a112aca
Mohamed Aaqib <aaqibakeel75@gmail.com>
2025-12-16 09:49:09 +0400

    setup created

---
+++ cpp-core/ChronOSCore/ChronOSCore/ChronOSCore.cpp
@@ -0,0 +1,12 @@
+// ChronOSCore.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include "utils/Hello.h"
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << getGreeting() << " this does work;!" << std::endl;
+    return 0;
+}
+
---
+++ cpp-core/src/utils/Hello.cpp
@@ -0,0 +1,6 @@
+#include "utils/Hello.h"
+#include "string";
+
+std::string getGreeting() {
+    return "Hello, World!";
+}
