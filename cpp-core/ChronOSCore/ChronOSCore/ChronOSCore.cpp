9ae15a9722f4561fdad4874cc7ec194acd76083d
Mohamed Aaqib <aaqibakeel75@gmail.com>
2025-12-22 21:40:58 +0400

    deleted buffer and parser, --understanding and reading code

--- cpp-core/ChronOSCore/ChronOSCore/ChronOSCore.cpp
+++ cpp-core/ChronOSCore/ChronOSCore/ChronOSCore.cpp
@@ -3,8 +3,6 @@
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
-#include "core/RingBuffer.h"
-#include "core/PCAPParser.h"
 #include "events/Event.h"
 
 using namespace std;
@@ -16,23 +14,15 @@ int main()
     sql::Connection* con;
     sql::Statement* stmt;
     sql::PreparedStatement* pstmt;
-    RingBuffer<Event, 8192> buffer;
 
 
     try
     {
-        PCAPParser pcap("data/http-chunked-gzip.pcap");
-        pcap.parse(buffer);
 
         //driver = get_driver_instance();
         //con = driver->connect("tcp://127.0.0.1:3306", "root", "");
         //cout << " this works \n";
         std::shared_ptr<Event> evt;
-        while (buffer.pop(evt)) {
-            std::cout << evt->serialize() << std::endl;
-        }
-
-        std::cout << "Parsed events: " << buffer.getSize() << std::endl;
         
     }
     catch (sql::SQLException e)
